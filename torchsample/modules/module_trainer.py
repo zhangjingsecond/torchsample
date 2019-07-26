0894cbb5fcda3b1f2ae76aa271f37a1ae8594acc
zhangjingsecond <43861357+zhangjingsecond@users.noreply.github.com>
2019-07-26 19:58:34 -0400

    Update module_trainer.py

--- torchsample/modules/module_trainer.py
+++ torchsample/modules/module_trainer.py
@@ -379,7 +379,7 @@ class ModuleTrainer(object):
                         metrics_logs = self.metric_container(output_batch, target_batch)
                         batch_logs.update(metrics_logs)
 
-                    batch_logs['loss'] = loss.data[0]
+                    batch_logs['loss'] = loss.data
                     callback_container.on_batch_end(batch_idx, batch_logs)
 
                 # TODO: Do this here or in callbacks.py?
@@ -508,7 +508,7 @@ class ModuleTrainer(object):
             loss = eval_loss_fn(output_batch, target_batch)
 
             samples_seen += batch_size
-            eval_logs['val_loss'] = (samples_seen*eval_logs['val_loss'] + loss.data[0]*batch_size) / (samples_seen+batch_size)
+            eval_logs['val_loss'] = (samples_seen*eval_logs['val_loss'] + loss.data*batch_size) / (samples_seen+batch_size)
 
             if self._has_metrics:
                 metrics_logs = metric_container(output_batch, target_batch)
@@ -549,7 +549,7 @@ class ModuleTrainer(object):
             loss = eval_loss_fn(output_batch, target_batch)
 
             samples_seen += batch_size
-            eval_logs['val_loss'] = (samples_seen*eval_logs['val_loss'] + loss.data[0]*batch_size) / (samples_seen+batch_size)
+            eval_logs['val_loss'] = (samples_seen*eval_logs['val_loss'] + loss.data*batch_size) / (samples_seen+batch_size)
 
             if self._has_metrics:
                 metrics_logs = metric_container(output_batch, target_batch)
